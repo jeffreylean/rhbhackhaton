c89b7a43c6054c082f92ecc334cf2c33a83ca88e
weifonglean <jeffrey@LAPTOP-OD3DPA3H.localdomain>
2021-06-12 00:57:44 +0800

    add cors policy

--- server/app.py
+++ server/app.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from server.routes.form import router as FormRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(FormRouter, tags=["Form"], prefix="/form")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["Root"])
 async def read_root():
